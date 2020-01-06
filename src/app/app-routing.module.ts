7f915d74d29f2be29bc64244da365b1098804d1e
wadekern <wade.kern@londonlife.com>
2020-01-06 15:42:15 -0500

    fixing routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,19 @@ import { RegisterComponent } from './register/register.component';
 import { LoginGaurd } from './login/login.gaurd';
 
 const routes: Routes = [
-  { path: '', component: RegisterComponent },
+  {
+    path: 'register', component: RegisterComponent,
+    children: [{
+      path: ':id', component: RegisterComponent
+    }],
+  },
+  {
+    path: '',
+    component: RegisterComponent
+  },
   { path: 'admin', component: AdminComponent, canActivate: [LoginGaurd] },
   { path: 'login', component: LoginComponent }
-  //{ path: 'staticPath', component: ... },
+  //{ path: 'staticPath', component: ... },   ptr
   //{ path: '**', component: ... },
   //{ path: 'oldPath', redirectTo: '/staticPath' },
   //{ path: ..., component: ..., data: { message: 'Custom' }
